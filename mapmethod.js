394a738640ba1a4b03dcfaf2796da8e02dba2977
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-18 16:39:22 -0300

    Finished lesson
    Finished lesson

--- mapmethod.js
+++ mapmethod.js
@@ -544,3 +544,16 @@ function checkPositive(arr) {
 
 checkPositive([1, 2, 3, -4, 5]);
 //----------------------------------------------
+function add(x) {
+    // Only change code below this line
+    return function (y) {
+        return function (z) {
+            return x + y + z;
+        }
+    }
+
+    // Only change code above this line
+}
+
+add(10)(20)(30);
+//----------------------------------------------
