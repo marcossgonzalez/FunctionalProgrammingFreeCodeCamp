d28a6f8b4441648e439b03c9340820c3a5d7449d
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-18 02:47:10 -0300

    Minor fix
    minor fix

--- slicemethod.js
+++ slicemethod.js
@@ -1,9 +1,9 @@
 //----------------------------------------------
 function sliceArray(anim, beginSlice, endSlice) {
-    // Only change code below this line
-    return anim.slice(beginSlice, endSlice);
+  // Only change code below this line
+  return anim.slice(beginSlice, endSlice);
 
-    // Only change code above this line
+  // Only change code above this line
 }
 
 const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
@@ -11,28 +11,28 @@ sliceArray(inputAnim, 1, 3);
 //----------------------------------------------
 function nonMutatingSplice(cities) {
 
-    return cities.slice(cities,3);
-  }
+  return cities.slice(cities, 3);
+}
 //----------------------------------------------
 function nonMutatingConcat(original, attach) {
-    // Only change code below this line
-    return original.concat(attach);
-  
-    // Only change code above this line
-  }
-  
-  const first = [1, 2, 3];
-  const second = [4, 5];
-  nonMutatingConcat(first, second);
+  // Only change code below this line
+  return original.concat(attach);
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
 //----------------------------------------------
 function nonMutatingPush(original, newItem) {
-    // Only change code below this line
-    return original.concat(newItem);
-  
-    // Only change code above this line
-  }
-  
-  const first2 = [1, 2, 3];
-  const second2 = [4, 5];
-  nonMutatingPush(first, second);
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+
+const first2 = [1, 2, 3];
+const second2 = [4, 5];
+nonMutatingPush(first, second);
 //----------------------------------------------
